7271b70bb6b2467c5611a4f74dc6908d7565acf5
ipodex <tomas.oderiz@gmail.com>
2025-06-04 20:43:05 -0300

    escenario basico y player movement basico

--- Proyecto motores y design/Assets/Scripts/PlayerMovement.cs
+++ Proyecto motores y design/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private float speed = 15f;
+    public float MinSpeed = 7f;
+    public float MaxSpeed = 25f;
+
+    public float JumpForce = 6f;
+
+    
+    public bool IsGrounded;
+    private Rigidbody rb;
     void Start()
     {
-        
+         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //Movimiento
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = MaxSpeed;
+        }
+        else
+        {
+            speed = MinSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        transform.Translate(new Vector3(x, 0, y) * Time.deltaTime * speed);
+    }
+
+    
+     public void Jump()
+    {
+        if (IsGrounded)
+        {
+                rb.AddForce(new Vector3(0, JumpForce, 0), ForceMode.Impulse);
+            }
+            
+
+        
+
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            IsGrounded = true;
+        }
+
         
     }
+    public void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            IsGrounded = false;
+        }
+    }
+     
+
+   
 }
