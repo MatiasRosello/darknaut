f0c9e122310d890212801344de5b7c333c286990
TheSebix <32584842+TheSebix@users.noreply.github.com>
2025-06-19 03:02:29 -0300

    Ajustes niveles, sonidos, nav mesh y animacion muerte
    Hice algunos ajustes a los niveles, acomode algunas cosas, objetos, parametros de los enemigos. Le puse el png de vidrio roto al trigger para que se vea. Ahora el player cuando le pegan hace un ruido de daño y cuando muere hace la animación de morir y despues cambia de pantalla. Puse enemigos con patrullas y algunos estaticos

--- Proyecto motores y design/Assets/Scripts/Player/PlayerHealth.cs
+++ Proyecto motores y design/Assets/Scripts/Player/PlayerHealth.cs
@@ -7,40 +7,46 @@ public class PlayerHealth : MonoBehaviour
 {
     [Header("Parámetros de Salud")]
     [SerializeField] private int maxHealth = 100;
-    [SerializeField] private int damageAmount = 50;
 
     [SerializeField] private int currentHealth;
+    [SerializeField] private AudioClip dyingSound;
+    [SerializeField] private AudioClip hurtSound;
 
-    // Eventos opcionales para UI u otros sistemas
-    public UnityEvent<int, int> OnHealthChanged; // pasa (current, max)
-    public UnityEvent OnPlayerDied;
+    private Animator animator;
+    private AudioSource audioSource;
 
+    // Eventos opcionales para UI
+    //public UnityEvent<int, int> OnHealthChanged; // pasa (current, max)
+    //public UnityEvent OnPlayerDied;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void Start()
     {
         currentHealth = maxHealth;
-        OnHealthChanged?.Invoke(currentHealth, maxHealth);
-    }
-
-    public void TakeHit()
-    {
-        TakeDamage(damageAmount);
+        //OnHealthChanged?.Invoke(currentHealth, maxHealth);
     }
 
     public void TakeDamage(int amount)
     {
+        audioSource.PlayOneShot(hurtSound);
         currentHealth = Mathf.Max(currentHealth - amount, 0);
-        OnHealthChanged?.Invoke(currentHealth, maxHealth);
+        //OnHealthChanged?.Invoke(currentHealth, maxHealth);
 
         if (currentHealth <= 0)
         {
-            Die();
+            audioSource.PlayOneShot(dyingSound);
+            animator.SetTrigger("Death");
         }
     }
     public void Die()
     {
         Debug.Log("[PlayerHealth] El jugador ha muerto.");
-        OnPlayerDied?.Invoke();
+        //OnPlayerDied?.Invoke();
         GameManager.Instance.LoadGameOver();
     }
 
